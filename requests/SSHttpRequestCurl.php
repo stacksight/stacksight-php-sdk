ef135b19748b07f1117f953a894330f0cf879384
Igor Kozlovsky <igor@linnovate.net>
2015-11-03 15:01:57 +0300

    Send cURL as single request

--- requests/SSHttpRequestCurl.php
+++ requests/SSHttpRequestCurl.php
@@ -6,20 +6,21 @@ class SSHttpRequestCurl extends SSHttpRequest implements SShttpInterface {
         $ch = ($url) ? curl_init(INDEX_ENDPOINT_01.$url) : curl_init(INDEX_ENDPOINT_01.'/'.$data['index'].'/'.$data['eType']);
         curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
         curl_setopt($ch, CURLOPT_POSTFIELDS, $data_string);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, false);
-//        curl_setopt($ch, CURLOPT_FRESH_CONNECT, true);
+        curl_setopt($ch, CURLOPT_FRESH_CONNECT, true);
 //        curl_setopt($ch, CURLOPT_TIMEOUT_MS, 10);
+        curl_setopt($ch, CURLOPT_USERAGENT, 'api');
+        curl_setopt($ch, CURLOPT_TIMEOUT, 1);
+        curl_setopt($ch, CURLOPT_HEADER, 0);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, false);
+        curl_setopt($ch, CURLOPT_FORBID_REUSE, true);
+        curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 1);
+        curl_setopt($ch, CURLOPT_DNS_CACHE_TIMEOUT, 10);
+
         curl_setopt($ch, CURLOPT_HTTPHEADER, array(
                 'Content-Type: application/json',
                 'Content-Length: ' . strlen($data_string))
         );
 
-        $response = curl_exec($ch);
-        if ($response !== false) {
-            return array('success' => true, 'message' => 'OK');
-        } else {
-            $error = error_get_last();
-            return array('success' => false, 'message' => $error['message']);
-        }
+        curl_exec($ch);
     }
 }
