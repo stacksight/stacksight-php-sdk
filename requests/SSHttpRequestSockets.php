48bb407b7749e948aaf219ee387c1ee2d0223d2b
Igor Kozlovsky <igor@linnovate.net>
2016-01-13 17:24:54 +0300

    Unstable connection write into log and don't show any warnings

--- requests/SSHttpRequestSockets.php
+++ requests/SSHttpRequestSockets.php
@@ -6,6 +6,10 @@ class SSHttpRequestSockets extends SSHttpRequest implements SShttpInterface {
     private $_socket;
 
     public $max_retry = 1;
+
+    private $_state_socket = false;
+
+    private $_socket_error = array();
     
     public function __construct(){
         $this->createSocket();
@@ -17,49 +21,69 @@ class SSHttpRequestSockets extends SSHttpRequest implements SShttpInterface {
 
     private function createSocket(){
         $flags = STREAM_CLIENT_ASYNC_CONNECT;
-        if(!$this->_socket)
-            $this->_socket = stream_socket_client($this->protocol . "://" . $this->host. ':' . $this->port, $errno, $errstr, $this->timeout, $flags);
-        stream_set_blocking($this->_socket, false);
+        if(!$this->_socket){
+            if($this->_socket = @stream_socket_client($this->protocol . "://" . $this->host. ':' . $this->port, $errno, $errstr, $this->timeout, $flags)){
+                stream_set_blocking($this->_socket, false);
+                $this->_state_socket = true;
+                $this->_socket_error = array();
+            } else{
+                $this->_socket_error = array(
+                    'error_num' => $errno,
+                    'error_message' =>  $errstr
+                );
+            }
+        }
+
     }
 
     private function closeSocket(){
-        fclose($this->_socket);
+        if($this->_state_socket === true)
+            fclose($this->_socket);
     }
 
     public function sendRequest($data, $url = false){
-        if($url === false)
-            $url = $this->api_path.'/'.$data['index'].'/'.$data['eType'];
-        else
-            $url = $this->api_path.$url;
+        if($this->_state_socket === true){
+            if($url === false)
+                $url = $this->api_path.'/'.$data['index'].'/'.$data['eType'];
+            else
+                $url = $this->api_path.$url;
 
-        $content = json_encode($data);
-        $req = "";
-        $req.= "POST /$url HTTP/1.1\r\n";
-        $req.= "Host: " . $this->host . "\r\n";
-        $req.= "Content-Type: application/json\r\n";
-        $req.= "Accept: application/json\r\n";
-        $req.= "Content-length: " . strlen($content) . "\r\n";
-        $req.= "\r\n";
-        $req.= $content;
+            $content = json_encode($data);
+            $req = "";
+            $req.= "POST /$url HTTP/1.1\r\n";
+            $req.= "Host: " . $this->host . "\r\n";
+            $req.= "Content-Type: application/json\r\n";
+            $req.= "Accept: application/json\r\n";
+            $req.= "Content-length: " . strlen($content) . "\r\n";
+            $req.= "\r\n";
+            $req.= $content;
 
-        if(!fwrite($this->_socket, $req)){
-            $sended = false;
-            for($i = 0; $i <= $this->max_retry; $i++){
-                usleep(200000);
-                if(fwrite($this->_socket, $req)){
-                    $sended = true;
-                    break;
+            if(!fwrite($this->_socket, $req)){
+                $sended = false;
+                for($i = 0; $i <= $this->max_retry; $i++){
+                    usleep(200000);
+                    if(fwrite($this->_socket, $req)){
+                        $sended = true;
+                        break;
+                    }
+                }
+                if($sended === false){
+                    $this->closeSocket();
+                    usleep(200000);
+                    $this->createSocket();
+                    if(!fwrite($this->_socket, $req)){
+                        $cURL = new SSHttpRequestCurl();
+                        $cURL->sendRequest($data, $url);
+                    }
                 }
             }
-            if($sended === false){
-                $this->closeSocket();
-                usleep(200000);
-                $this->createSocket();
-                if(!fwrite($this->_socket, $req)){
-                    $cURL = new SSHttpRequestCurl();
-                    $cURL->sendRequest($data, $url);
-                }
+        } else {
+            if(!empty($this->_socket_error)){
+                $error_num = $this->_socket_error['error_num'];
+                $error_message = $this->_socket_error['error_message'];
+                SSUtilities::error_log("#$error_num: $error_message", 'error_socket_connection');
             }
         }
+
     }
 }
