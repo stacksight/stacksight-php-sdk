e6dc33d1516d8f3c14846b67d9286a746cb8f56c
Igor Kozlovsky <igor@linnovate.net>
2016-04-19 11:44:58 +0300

    Return log write function Add attribute for refresh log

--- SSUtilities.php
+++ SSUtilities.php
@@ -37,18 +37,23 @@ class SSUtilities {
 		}
 	}
 
-	static function error_log($message, $level = 'info') {
+	static function error_log($message, $level = 'info', $refresh = false) {
 		if (!$message || (!defined('STACKSIGHT_DEBUG') || (defined('STACKSIGHT_DEBUG') && STACKSIGHT_DEBUG !== true))) return;
 	    if (is_array($message) || is_object($message)) $message = print_r($message, true);
 
 	    $log_file = dirname(__FILE__).'/../'.$level.'.log';
 	    // delete logfile if filesize more than $logfile_limit
+
+
 	    $logfile_limit = 1024000; // размер лог файла в килобайтах (102400 = 100 мб)
-	    if (file_exists($log_file) && filesize($log_file) / 1024 > $logfile_limit) unlink($log_file);
+	    if ((file_exists($log_file) && filesize($log_file) / 1024 > $logfile_limit) || (file_exists($log_file) && $refresh === true))
+			unlink($log_file);
 	    
 	    // $date = new Datetime(null, new DateTimeZone('Europe/Minsk'));
 	    $date = new Datetime();
 	    $date_format = $date->format('d.m.Y H:i:s');
+
+		error_log($date_format .' '. $message."\n", 3, $log_file);
 	}
 
 	static function t($str, $params = array()) {
