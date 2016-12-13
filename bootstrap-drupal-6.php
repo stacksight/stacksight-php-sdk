c496c07098e6d9d52bf57ad4db3550447896eb4d
Igor Kozlovsky <igor@linnovate.net>
2016-12-13 13:33:19 +0300

    Return APP_ID & TOKEN define params

--- bootstrap-drupal-6.php
+++ bootstrap-drupal-6.php
@@ -14,7 +14,7 @@ require_once('platforms/SSDrupalClient.php');
 global $ss_client;
 define('STACKSIGHT_INIT_START', TRUE);
 
-define('DOCS_URL', '#');
+define('DOCS_URL', 'http://stacksight.io/docs/#wordpress-installation');
 
 if(defined('STACKSIGHT_TOKEN')){
     if(defined('STACKSIGHT_APP_ID'))
--- bootstrap-wp.php
+++ bootstrap-wp.php
@@ -62,7 +62,8 @@ class WPBootstrap{
 
 	public function init(){
 		if($this->ready == true){
-			$defines_from_db = $this->defineVars();
+            define('STACKSIGHT_SETTINGS_IN_DB', true);
+            $defines_from_db = $this->defineVars();
 			if(is_array($defines_from_db) && !empty($defines_from_db)){
 				foreach($defines_from_db as $key => $config_section){
 					if(is_array($config_section) && !empty($config_section)){
